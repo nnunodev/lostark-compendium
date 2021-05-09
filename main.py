d1f38a463d15cf96ef7ec4ea02b4686e8567b0c3
Nuno Nunes <nunotnunes@outlook.com>
2021-05-09 17:59:33 +0100

    added server region

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import flask, render_template, request
+from flask import Flask, render_template, request
+import aws as aws
 import boto3
 
 
-app = flask(__name__)
+app = Flask(__name__)
 
 # db server settings
 
 dynamodb = boto3.resource('dynamodb',
-                          aws_access_key_id=ACCESS_KEY,
-                          aws_secret_access_key=SECRET_KEY,
-                          aws_session_token=SESSION_TOKEN)
+                          aws_access_key_id=aws.ACCESS_KEY,
+                          aws_secret_access_key=aws.SECRET_KEY,
+                          region_name = aws.REGION
+                          )
+
 
 @app.route('/')
-    def index():
-        return(render_template('index.html'))
+def index():
+    return(render_template('index.html'))
+ 
