c314411928d6cf9ece3c17c5c78b344165eb60af
Nuno Nunes <nunotnunes@outlook.com>
2021-05-09 17:45:40 +0100

    added dynamodb keys and server settings

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import flask, render_template, request
+import boto3
+
+
+app = flask(__name__)
+
+# db server settings
+
+dynamodb = boto3.resource('dynamodb',
+                          aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY,
+                          aws_session_token=SESSION_TOKEN)
 
 @app.route('/')
-def test():
-    return 'test!'
+    def index():
+        return(render_template('index.html'))
